4ce91c66ebd0fbd291f65b8afc1bbf0fa4ecf36c
Pravin Prabhu <prabhu.pravin@gmail.com>
2017-01-28 14:22:41 +0530

    Initial commit of Spring Boot application

---
+++ mock-server-api/src/main/java/com/pravin/MockServerApiApplication.java
@@ -0,0 +1,12 @@
+package com.pravin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockServerApiApplication.class, args);
+	}
+}
